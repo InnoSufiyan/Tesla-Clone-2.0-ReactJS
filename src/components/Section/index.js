5dcfa50a5415dd86259e8802179143297142370b
unknown <innosufiyan@gmail.com>
2021-11-29 21:22:36 +0500

    second commit

---
+++ src/components/Section/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+    return (
+        <Container>
+            Section
+        </Container>
+    )
+}
+
+export default Section
+
+
+const Container = styled.div`
+    height: 100vh;
+`
