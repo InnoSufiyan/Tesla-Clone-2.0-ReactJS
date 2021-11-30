5d0c0922f8f81720f9fd0d417585a384888e9dae
innosufiyan <innosufiyan@gmail.com>
2021-11-30 12:50:06 +0500

    30 november

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,54 @@ import Section from '../Section'
 function Home() {
     return (
         <Container>
-            <Section/>
+            <Section
+                title="Model S"
+                description="Order online for touchless delivery"
+                backgroundImg= 'model-s.jpg'
+                leftBtnTxt= 'Custom Order'
+                rightBtnTxt = 'Existing Inventory'
+            />
+            <Section
+                title="Model Y"
+                description="Order online for touchless delivery"
+                backgroundImg= 'model-y.jpg'
+                leftBtnTxt= 'Custom Order'
+                rightBtnTxt = 'Existing Inventory'
+            />
+            <Section
+                title="Model 3"
+                description="Order online for touchless delivery"
+                backgroundImg= 'model-3.jpg'
+                leftBtnTxt= 'Custom Order'
+                rightBtnTxt = 'Existing Inventory'
+            />
+            <Section
+                title="Model X"
+                description="Order online for touchless delivery"
+                backgroundImg= 'model-x.jpg'
+                leftBtnTxt= 'Custom Order'
+                rightBtnTxt = 'Existing Inventory'
+            />
+            <Section
+                title="Lower Cost Panels in America"
+                description="Money back guarantee"
+                backgroundImg= 'solar-panel.jpg'
+                leftBtnTxt= 'Order Now'
+                rightBtnTxt = 'Learn More'
+            />
+            <Section
+                title="Solar New Roof"
+                description="Solar Roof costs Less than a New Roof"
+                backgroundImg= 'solar-roof.jpg'
+                leftBtnTxt= 'Order Now'
+                rightBtnTxt = 'Learn More'
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg= 'accessories.jpg'
+                leftBtnTxt= 'Order Now'
+            />
         </Container>
     )
 }
