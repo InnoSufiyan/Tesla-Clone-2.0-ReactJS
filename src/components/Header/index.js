5e9074856c15c3d5ebf12d969bc0cad3a144c0a6
inno sufiyan <innosufiyan@gmail.com>
2021-12-03 18:23:27 +0500

    3 december

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu'
 
 function Header() {
     return (
@@ -16,6 +17,7 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
+                <CustomMenu />
             </RightMenu>
         </Container>
     )
@@ -29,6 +31,7 @@ const Container = styled.div`
     position: fixed;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 0 20px;
     // width: 100vw;
     top: 0;
@@ -42,17 +45,27 @@ const Menu = styled.div`
     flex :1;
     justify-content: center;
 
-    p {
+    a {
         font-width: 600;
         text-transform: uppercase;
         padding: 0 10px;
         flex-wrap: nowrap;
     }
+    @media(max-width: 768px) {
+        display: none;
+    }
 `
 const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
     a {
         font-weight: 600;
         text-transform: uppercase;
         margin-right: 10px;
     }
+`
+
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+
 `
